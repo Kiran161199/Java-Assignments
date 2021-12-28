ed13fa87120fcc344b627e4d7e5d6e070b8dafce
Kiran161199 <76390384+Kiran161199@users.noreply.github.com>
2021-12-28 06:13:26 +0530

    Add files via upload

---
+++ day 2 assignment/MainCalculator.java
@@ -0,0 +1,27 @@
+package com.rocky.program;
+
+public class MainCalculator {
+	public static void main(String[] args) {
+		Calculator cal=new Calculator();
+		cal.add(10, 20);
+		cal.add(10.0,20.0);
+		cal.add(10, 20.0);
+		cal.add(10.0, 20);
+		System.out.println("-----------------------------------");
+		cal.diff(10, 20);
+		cal.diff(10.0,20.0);
+		cal.diff(10, 20.0);
+		cal.diff(10.0, 20);
+		System.out.println("-----------------------------------");
+		cal.mul(10, 20);
+		cal.mul(10.0,20.0);
+		cal.mul(10, 20.0);
+		cal.mul(10.0, 20);
+		System.out.println("-----------------------------------");
+		cal.div(10, 20);
+		cal.div(10.0,20.0);
+		cal.div(10, 20.0);
+		cal.div(10.0, 20);
+		System.out.println("-----------------------------------");
+	}
+}
