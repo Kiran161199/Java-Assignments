a3d08d5b64b07031f60725676f8dc8555410b29d
Kiran161199 <76390384+Kiran161199@users.noreply.github.com>
2021-12-28 05:57:58 +0530

    Add files via upload

---
+++ day 2 assignment/MainEmployee (1).java
@@ -0,0 +1,10 @@
+package com.kiran.program;
+
+public class MainEmployee {
+	public static void main(String[] args) {
+		Employee emp=new Employee("kiran");
+		Employee emp1=new Employee("Rahul",100);
+		Employee emp2=new Employee("Ram",100,22,"Junior Associate","Banglore",5);
+		
+	}
+}
---
+++ day 2 assignment/MainShape (1).java
@@ -0,0 +1,9 @@
+package com.kiran.programs;
+
+public class MainShape {
+	public static void main(String[] args) {
+		Shape shape1=new Shape();
+		shape1.perimeter(10, 20);
+		shape1.perimeter(10);	
+	}
+}
