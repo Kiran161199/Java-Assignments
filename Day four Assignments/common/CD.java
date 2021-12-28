aa7e15036d5ec51e45407b6d2fa84e160da5f453
Kiran161199 <76390384+Kiran161199@users.noreply.github.com>
2021-12-28 06:16:29 +0530

    Add files via upload

---
+++ Day four Assignments/common/CD.java
@@ -0,0 +1,15 @@
+package com.common;
+
+public class CD {
+	String title;
+	String singer;
+	public CD(String title, String singer) {
+		this.title = title;
+		this.singer = singer;
+	}
+	@Override
+	public String toString() {
+		return " title=" + title + ", singer=" + singer +"" ;
+	}
+	
+}
---
+++ Day four Assignments/common/SortCdBySinger.java
@@ -0,0 +1,13 @@
+package com.common;
+
+import java.util.Comparator;
+
+public class SortCdBySinger implements Comparator<CD> {
+
+	@Override
+	public int compare(CD o1, CD o2) {
+		// TODO Auto-generated method stub
+		return o2.singer.compareTo(o1.singer);
+	}
+
+}
